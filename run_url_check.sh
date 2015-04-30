9abed3248be59f2fc3e9fc93f380f365d1da143b
Alexander Savelyev <aleksandr_savelev@epam.com>
2015-04-30 15:56:03 +0300

    add internal only link checker

--- run_url_check.sh
+++ run_url_check.sh
@@ -4,4 +4,4 @@ echo "Run cross-reference verify"
 
 cd build/html
 
-linkchecker --ignore-url=^http --ignore-url=^todo -t 100 -F html index.html
+linkchecker --ignore-url=^todo -t 100 -F html index.html
