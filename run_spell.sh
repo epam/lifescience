6903dfeb8d46a7f1e50165f98c8fca3926c3aaac
Alexander Savelyev <aleksandr_savelev@epam.com>
2015-11-16 15:26:16 +0300

    update spell dictionary and run script

--- run_spell.sh
+++ run_spell.sh
@@ -2,11 +2,13 @@
 
 echo " " > aspell_output.txt
 
-for f in rst/*/*.rst 
+FILES=$(find rst/ -type f -name '*.rst')
+
+for f in $FILES
 do
    echo $f
    echo "******************************************************************************************************************************" >> aspell_output.txt
    echo $f >> aspell_output.txt
    echo "******************************************************************************************************************************" >> aspell_output.txt
-   cat $f | aspell  pipe list -d en_GB  ignore,-W 4 --encoding=utf-8 --mode=tex --personal=./aspell.ignore.txt  | grep -E "\&.*|\# .*" >> aspell_output.txt
+   cat $f | aspell  pipe list -d en_US  --ignore=4 --encoding=utf-8 --mode=tex --personal=./aspell.ignore.txt  | grep -E "\&.*|\# .*" >> aspell_output.txt
 done
