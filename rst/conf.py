6a5892e6da989ed5ac619bdd41bb87b8c8e96ce8
Alexander Savelyev <aleksandr_savelev@epam.com>
2016-02-15 16:29:40 +0300

    change copyright

--- rst/conf.py
+++ rst/conf.py
@@ -55,7 +55,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Life Sciences'
-copyright = u'2015, EPAM Systems'
+copyright = u'2016, EPAM Systems'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
