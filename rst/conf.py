c264dc6cfe95de26a99cab5bfc6a1c9e948abcb7
Aleksandr Savelev <Aleksandr_Savelev@epam.com>
2017-02-13 19:09:15 +0300

    add favicon

--- rst/conf.py
+++ rst/conf.py
@@ -147,7 +147,7 @@ html_last_updated_fmt = '%b %d, %Y'
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-html_favicon = 'assets/favicon.ico'
+# html_favicon = 'assets/favicon.ico'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
