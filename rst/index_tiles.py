ddba4284544c072be9e8d7be6eb74a99bb11dafa
Aleksandr Savelev <Aleksandr_Savelev@epam.com>
2021-01-07 12:15:08 +0000

    add index tiles layout

--- rst/index_tiles.py
+++ rst/index_tiles.py
@@ -1,17 +1,56 @@
-def index_tiles(s):
-    # print(s)
+import xml.etree.ElementTree as ET
 
+def index_tiles(data: str):
+    # print(s)
+    # with open ('tiles.xml', 'w') as f:
+    #     f.write(s)
+
+    data_root = ET.fromstring(data.encode('utf-8'))
     res_items = []
 
-    res_items.append({
-        'path': '1',
-        'image': '_images/Bingo.jpg',
-        'label': 'Label',
-        'title': 'Title',
-        'text': 'Text',
-        'description': '<li>Description</li>'
-    })
+    for elem in data_root:
+        if 'class' in elem.attrib and elem.attrib['class'] == 'section':
+            label_name = ''
+            
+            for section in elem:
+                if section.tag == 'h2':
+                    label_name = section.text
+                    
+                if section.tag == 'ul':
+                    for product in section:
+                        title = ''
+                        path = ''
+                        descr = ''
+                        image = ''
+                        text = ''
 
+                        product_p = product.find('p')
+                        if product_p is not None:
+                            for p in product_p:
+                                if p.tail is not None:
+                                    text = p.tail
+                            product_a = product_p.find('a')
+                            if product_a is not None:
+                                path = product_a.attrib['href']
+                                title = product_a.text
+                            product_img = product_p.find('img')
+                            if product_img is not None:
+                                image = product_img.attrib['src']
+                        
+
+                        product_ul = product.find('ul')
+                        if product_ul is not None:
+                            descr = ET.tostring(product_ul, encoding="UTF-8", method="html").decode('utf-8')
+
+
+                        res_items.append({
+                            'path': path,
+                            'image': image,
+                            'label': label_name,
+                            'title': title,
+                            'text': text,
+                            'description': descr
+                        })
     return res_items
 
 def add_filters(app):
@@ -19,3 +58,12 @@ def add_filters(app):
 
 def setup(app):
     app.connect("builder-inited", add_filters)
+
+def local_test():
+    with open('tiles.xml', 'r') as f:
+        s = index_tiles(f.read())
+        print(s)
+
+
+if __name__ == '__main__':
+    local_test()
--- rst/themes/lifescience-theme/static/js/index-tiles.js
+++ rst/themes/lifescience-theme/static/js/index-tiles.js
@@ -1,83 +1,10 @@
 $(document).ready(function() {
     var indexBody = document.getElementById("life-sciences-open-source");
     if (!indexBody) return;
-    var data = getTiles(indexBody);
+    // $('#index-tiles').append(template({items: data}));
+    // data.forEach(function(tile){
+    //     // console.log(tile);
+    //     $(".tile-click-"+tile["path"]).click(function(){location.href=tile["href"]});
+    //  });
 
-    //console.log(data);
-
-    var htmlstring =
-        '{{#each items}}' +
-        '   <div class="col-xs-12 col-sm-6">' +
-        '       <div class="tile tile-click-{{path}}">' +
-        '           <div class="tile-image">' +
-        '           <img src="{{image}}" alt="">' +
-        '           </div>' +
-        '           <div class="tile-content">' +
-        '               <div class="tile-text">' +
-        '                   <div class="tile-label">{{label}}</div>' +
-        '                   <h3>{{title}}</h3>' +
-        '                   <p>{{{text}}}</p>' +
-        '                   <ul class="tile-description">' +
-        '                       {{{description}}}' +
-        '                   </ul>' +
-        '               </div>' +
-        '           </div>' +
-        '       </div>' +
-        '   </div>' +
-        '{{/each}}';
-
-    var template = Handlebars.compile(htmlstring);
-    $('#index-tiles').append(template({items: data}));
-    data.forEach(function(tile){
-        // console.log(tile);
-        $(".tile-click-"+tile["path"]).click(function(){location.href=tile["href"]});
-     });
-
-    function getTiles(current) {
-        var tiles = [];
-        var child;
-        for (var i = 0; i < current.children.length; i++) {
-            child = current.children[i];
-            if (child.localName != 'div' && !child.classList.contains('section'))
-                continue;
-
-            var label, labelName;
-
-            for (var j = 0; j < child.children.length; j++) {
-                label = child.children[j];
-
-                if (label.localName == 'h2') labelName = label.innerText.replace('¶', '');
-                if (label.localName == 'ul') {
-
-                    for (var k = 0; k < label.children.length; k++) {
-                        tiles.push(createTile(label.children[k], labelName));
-                    }
-                }
-            }
-        }
-        return tiles;
-    }
-
-    function createTile(tmp, labelName) {
-        var tile = {};
-        tile['label'] = labelName;
-        tile['href'] = tmp.childNodes[0].href;
-        tile['title'] = tmp.childNodes[0].innerText;
-        tile['text'] = '';
-        tile['path'] = tmp.childNodes[0].innerText.split(" ").join("");
-        for (var index = 1; index < tmp.childNodes.length; index++) {
-            var elem = tmp.childNodes[index];
-            if (elem.localName == 'ul') {
-                tile['description'] = elem.innerHTML;
-                continue;
-            }
-            if (elem.localName == 'img') {
-                tile['image'] = elem.src;
-                continue;
-            }
-            if (elem.outerHTML) tile['text'] += elem.outerHTML;
-            else tile['text'] += elem.data;
-        }
-        return tile;
-    }
 });
