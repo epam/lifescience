8d2c8f876ce9e2edbcdb226654eaa7f9be217b4c
Aleksandr Savelev <Aleksandr_Savelev@epam.com>
2021-09-26 14:31:49 +0300

    update indigo to latest version

--- indigo/download.sh
+++ indigo/download.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 indigo_head=`cat HEAD`
 
-wget https://github.com/epam/Indigo/raw/$indigo_head/api/indigo.h
-wget https://github.com/epam/Indigo/raw/$indigo_head/api/python/indigo.py
-wget https://github.com/epam/Indigo/raw/$indigo_head/api/plugins/renderer/python/indigo_renderer.py
-wget https://github.com/epam/Indigo/raw/$indigo_head/api/plugins/inchi/python/indigo_inchi.py
+wget https://github.com/epam/Indigo/raw/$indigo_head/api/c/indigo/indigo.h
+wget https://github.com/epam/Indigo/raw/$indigo_head/api/python/indigo/__init__.py -O indigo.py
+wget https://github.com/epam/Indigo/raw/$indigo_head/api/python/indigo/renderer.py -O indigo_renderer.py
+wget https://github.com/epam/Indigo/raw/$indigo_head/api/python/indigo/inchi.py -O indigo_inchi.py
