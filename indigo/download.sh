f4a3ce43b442a27b0db78104029390a33d22bc05
Aleksandr Savelev <aleksandr_savelev@epam.com>
2025-09-02 15:29:50 +0300

    fix indigo upload (#159)

--- indigo/download.sh
+++ indigo/download.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
-indigo_head=`cat HEAD`
 
-wget https://github.com/epam/Indigo/raw/$indigo_head/api/c/indigo/indigo.h
-wget https://github.com/epam/Indigo/raw/$indigo_head/api/python/indigo/__init__.py -O indigo.py
-wget https://github.com/epam/Indigo/raw/$indigo_head/api/python/indigo/renderer.py -O indigo_renderer.py
-wget https://github.com/epam/Indigo/raw/$indigo_head/api/python/indigo/inchi.py -O indigo_inchi.py
+pip3 install epam.indigo
+
+wget https://raw.githubusercontent.com/epam/Indigo/master/api/c/indigo/indigo.h -O indigo.h
